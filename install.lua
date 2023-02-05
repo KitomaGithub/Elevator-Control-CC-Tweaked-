92a0eae100aca81495758ac4b6f1dcd1c85c4690
kitomahiragashi <hvahbmspq24hlhdpixluzgyopgnkfi5eybwahltri4se43vuhevq>
2023-02-05 09:54:54 +0800

    Temp Intall Code

--- install.lua
+++ install.lua
@@ -1,19 +1,27 @@
--- if fs.exists("disk/modules") then
---     fs.delete("disk/modules")
--- end
+if fs.exists("disk/modules") then
+    fs.delete("disk/modules")
+end
  
--- destination = "disk/modules/"
--- program = "pastebin"
--- command = "get"
+destination = "disk/modules/"
+program = "wget"
 
--- shell.run( program, command, "yS6WtBGJ", destination .. "elevMove" )
--- shell.run( program, command, "xj8dATfq", destination .. "network" )
--- shell.run( program, command, "uDFZey2b", destination .. "fileSys" )
--- shell.run( program, command, "f9bjVNKz", destination .. "textC" )
+draw = "https://raw.githubusercontent.com/KitomaGithub/Elevator-Control-CC-Tweaked-/master/modules/draw.lua"
+elevMove = "https://raw.githubusercontent.com/KitomaGithub/Elevator-Control-CC-Tweaked-/master/modules/elevMove.lua"
+fileSys = "https://raw.githubusercontent.com/KitomaGithub/Elevator-Control-CC-Tweaked-/master/modules/fileSys.lua"
+network = "https://raw.githubusercontent.com/KitomaGithub/Elevator-Control-CC-Tweaked-/master/modules/network.lua"
+textC = "https://raw.githubusercontent.com/KitomaGithub/Elevator-Control-CC-Tweaked-/master/modules/textC.lua"
 
--- shell.run( program, command, "2ZwpF5zi", destination .. "draw" )
+serverFile = "https://raw.githubusercontent.com/KitomaGithub/Elevator-Control-CC-Tweaked-/master/server_control.lua"
+
+shell.run( program, elevMove, destination .. "elevMove.lua" )
+shell.run( program, draw, destination .. "draw.lua" )
+shell.run( program, fileSys, destination .. "fileSys.lua" )
+shell.run( program, network, destination .. "network.lua" )
+shell.run( program, textC, destination .. "textC.lua" )
+
+shell.run( program, serverFile, "server_control.lua" )
 
 term.clear()
 term.setCursorPos(1,1)
 
-print("Test")
+print("Program successfully Installed")
